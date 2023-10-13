4c18c4c24911c19af345c723184b5342bf8bde69
ADEMJAZI <116764724+ADEMJAZI@users.noreply.github.com>
2023-10-13 22:21:49 +0100

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    String name;
+    String Firstname;
+    int daynaiss;
+    int monthnaiss;
+    int yearnaiss;
+
+    void setname(String name){
+        this.name=name;
+    }
+    void setFirstName(String Firstname){
+        this.Firstname=Firstname;
+    }
+    void datenaiss(int dd , int mm , int yy){
+        this.daynaiss=dd;
+        this.monthnaiss=mm;
+        this.yearnaiss=yy;
+
+    }
+    int AgeEn2023(){
+        return (2023-yearnaiss);
+
+    }
+    public static void main(String[] args){
+        Person p1=new Person();
+        p1.setname("adem");
+        p1.setFirstName("jazi");
+        p1.datenaiss(6,9,2000);
+
+        System.out.println("name : "+p1.name);
+        System.out.println("name : "+p1.Firstname);
+        System.out.println("date de naissance : "+p1.daynaiss+"/"+p1.monthnaiss+"/"+p1.yearnaiss);
+        System.out.println("age : "+p1.AgeEn2023());
+    }
+
+}
